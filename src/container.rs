8dcf4140c4035b7507e164a55e05fab29950a3f4
Jyothish V S <jsvidyad@syr.edu>
2024-02-13 12:21:56 +0530

    First working version

--- src/container.rs
+++ src/container.rs
@@ -1,1 +1,27 @@
-use std::ops::{}
+use std::fmt::Debug;
+use std::ops::{Deref, DerefMut};
+
+#[derive(Debug, Clone)]
+pub struct Container<T> {
+    elements: (T, T, T),
+}
+
+impl<T> Container<T> {
+    pub fn new(elements: (T, T, T)) -> Self {
+        Self { elements }
+    }
+}
+
+impl<T> Deref for Container<T> {
+    type Target = (T, T, T);
+
+    fn deref(&self) -> &Self::Target {
+        &self.elements
+    }
+}
+
+impl<T> DerefMut for Container<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.elements
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod container;
+use container::Container;
+
 fn main() {
-    println!("Hello, world!");
+    let mut c = Container::new((1, 2, 3));
+    println!("{:?}", c);
+    println!("{}", c.0);
+    c.0 = 5;
+    println!("{:?}", c);
+    println!("{}", c.0);
 }
